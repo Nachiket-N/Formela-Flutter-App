94aa9ff87fb9e9bfa538d4139e9196a73d7485a4
Nachiket-N <nachiket.n.naik@gmail.com>
2021-10-25 17:34:18 +0530

    modified settingspage

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -16,14 +18,39 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: formelaAppBar(titleName: "SETTINGS"),
+      backgroundColor: const Color.fromRGBO(65, 62, 75, 0.9),
       body: Center(
-        child: MaterialButton(
-          color: Colors.yellowAccent,
-          onPressed: () async {
-            await FirebaseAuth.instance.signOut();
-            Navigator.pushNamed(context, '/landingPage');
-          },
-          child: Text('Sign Out'),
+        child: Container(
+          height: 50.0,
+          child: GestureDetector(
+            onTap: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushNamed(context, '/landingPage');
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(255, 189, 89, 1),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Center(
+                    child: Text(
+                      "LOGOUT",
+                      style: TextStyle(
+                        color: Color.fromRGBO(65, 62, 75, 1),
+                        fontFamily: 'Montserrat',
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
