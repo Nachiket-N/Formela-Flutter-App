390eb7a4b3895664a7a7ca5f4f2fcae566ea5910
Nachiket-N <nachiket.n.naik@gmail.com>
2021-11-30 15:23:12 +0530

    misce

--- lib/components/Pending Form/customedtext.dart
+++ lib/components/Pending Form/customedtext.dart
@@ -19,11 +19,12 @@ class _CustomizedTextState extends State<CustomizedText> {
       child: Text(
         widget.inputText,
         style: const TextStyle(
-            color: Color.fromRGBO(65, 62, 75, 1),
-            fontFamily: 'Montserrat',
-            fontSize: 16,
-            fontWeight: FontWeight.w800,
-            height: 1),
+          color: Color.fromRGBO(65, 62, 75, 1),
+          fontFamily: 'Montserrat',
+          fontSize: 16,
+          fontWeight: FontWeight.w800,
+          height: 1,
+        ),
       ),
     );
   }
